0e27c38823b5b81378b196420ac80101300b59f7
Maxim Kazachuk <dem2bb1@gmail.com>
2021-01-25 22:45:31 +0200

    Added 2 verisons class + functions

--- task.js
+++ task.js
@@ -32,3 +32,46 @@ function updateClockFace(time) {
 function pad(value) {
   return String(value).padStart(2, '0');
 }
+
+/*
+ *                         ВАРИАНТ ЧЕРЕЗ КЛАСС НИЖЕ
+ */
+
+// class CountdownTimer {
+//   constructor({ selector, targetDate }) {
+//     this.selector = selector;
+//     this.targetDate = targetDate;
+//   }
+
+//   startTimer() {
+//     setInterval(() => {
+//       const currentTime = new Date();
+//       const deltaTime = this.targetDate - currentTime;
+//       this.updateClockFace(deltaTime);
+//     }, 0);
+//   }
+
+//   updateClockFace(time) {
+//     const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+//     const hours = this.pad(
+//       Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+//     );
+//     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+//     refs.daysRef.textContent = `${days}`;
+//     refs.hoursRef.textContent = `${hours}`;
+//     refs.minsRef.textContent = `${mins}`;
+//     refs.secsRef.textContent = `${secs}`;
+//   }
+
+//   pad(value) {
+//     return String(value).padStart(2, '0');
+//   }
+// }
+
+// const countDownNew = new CountdownTimer({
+//   selector: '#timer-1',
+//   targetDate: new Date('February 10, 2021'),
+// });
+
+// countDownNew.startTimer();
